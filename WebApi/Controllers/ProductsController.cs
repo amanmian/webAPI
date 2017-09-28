ca7af4dcd71a08a746ef697dbc0ca8f376962fa5
amanmian <amanmian.ksp@gmail.com>
2017-09-28 10:09:55 +0530

    web api

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        
+        context.Products.AddOrUpdate(p => p.ProductId,
+                new Product { Name = "Product 1", Category = "Category 1", Price = 200 },
+                new Product { Name = "Product 2", Category = "Category 2", Price = 500 },
+                new Product { Name = "Product 3", Category = "Category 3", Price = 700 }
+                );
+
+        context.Reviews.AddOrUpdate(r => r.ReviewId,
+                new Review {Title = "Review 1", Description = "Test review 1", ProductId = 1},
+                new Review {Title = "Review 2", Description = "Test review 2", ProductId = 1}
+                );
+    }
+}
---
+++ WebApi/Models/ProductsContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WebApi.Models
+{
+    public class DetailContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        //   public DbSet<NameStateCity> Names { get; set; }
+        //  public DbSet<Course> Courses { get; set; }
+        public DetailContext() : base("Aman")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("web");
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }   }
